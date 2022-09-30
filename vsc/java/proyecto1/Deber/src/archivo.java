c2d2c4a6c15a47f7dda194d8a06043ab2f95abe0
dereyes5 <107556653+dereyes5@users.noreply.github.com>
2022-09-30 02:53:40 -0500

    basura

--- vsc/java/proyecto1/Deber/src/archivo.java
+++
@@ -1,5 +0,0 @@
-public class archivo {
-    public static void main(String[] args) {
-        
-    }
-}
--- vsc/java/proyecto1/proyecto_2/src/calculadora.java
+++
@@ -1,7 +0,0 @@
-public class calculadora {
-    public static void main(String[] args) {
-        int a;
-        int b;
-        
-    }
-}
